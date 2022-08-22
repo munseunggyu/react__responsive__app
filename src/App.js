dd83209046ed4508a0c8c32e4bf82062d8852a3b
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-08-22 14:58:09 +0900

    Start React responsive app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import img from './img/img01.jpg'
+
+function Home(){
+  return(
+    <>
+      <Header />
+    </>
+  )
 }
 
-export default App;
+export default Home;
