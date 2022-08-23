2eaf09442d37aeb76a72ffc433c280ea1447d926
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-08-23 17:19:41 +0900

    Add Main Title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Header from './components/Header';
+import MainTitle from './components/MainTitle';
 import img from './img/img01.jpg'
 
 function Home(){
   return(
     <>
       <Header />
+      <MainTitle />
     </>
   )
 }
---
+++ src/components/MainTitle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const MainTitleContainer = styled.div`
+  width:100%;
+  height:70px;
+  background-color:#E9F7FD;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  h3{
+    color:#1C97C0;
+    font-size:40px;
+    font-family: 'Nanum Pen Script', cursive;
+  }
+`;
+
+function MainTitle(){
+  return(
+    <MainTitleContainer>
+      <h3>
+        "나는 개발자다!"
+      </h3>
+    </MainTitleContainer>
+  )
+}
+
+export default MainTitle;
